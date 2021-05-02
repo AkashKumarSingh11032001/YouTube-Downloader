8edd9d8c3a8dd73c7bba5e2c29876842d19b0f0d
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-05-02 19:49:24 +0530

    code

--- YouTube Downloader/code.py
+++ YouTube Downloader/code.py
@@ -1,27 +1,27 @@
 #import the package
 from pytube import YouTube
 
-url = 'Your URL goes here'
-my_video = YouTube(url)
+# url = 'Your URL goes here'
+# my_video = YouTube(url)
 
-print("*********************Video Title************************")
-#get Video Title
-print(my_video.title)
+# print("*********************Video Title************************")
+# #get Video Title
+# print(my_video.title)
 
-print("********************Tumbnail Image***********************")
-#get Thumbnail Image
-print(my_video.thumbnail_url)
+# print("********************Tumbnail Image***********************")
+# #get Thumbnail Image
+# print(my_video.thumbnail_url)
 
-print("********************Download video*************************")
-#get all the stream resolution for the 
-for stream in my_video.streams:
-    print(stream)
+# print("********************Download video*************************")
+# #get all the stream resolution for the 
+# for stream in my_video.streams:
+#     print(stream)
 
-#set stream resolution
-my_video = my_video.streams.get_highest_resolution()
+# #set stream resolution
+# my_video = my_video.streams.get_highest_resolution()
 
-#or
-#my_video = my_video.streams.first()
+# #or
+# #my_video = my_video.streams.first()
 
-#Download video
-my_video.download()
+# #Download video
+# my_video.download()
